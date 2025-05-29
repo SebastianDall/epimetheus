cf95dda304bf12a85055acceec5ee7f6384fd7a5
sebdal <semoda@bio.aau.dk>
2025-05-29 06:37:17 +0000

    chore: Removed unsued dead code

--- src/motif_clustering/mod.rs
+++ src/motif_clustering/mod.rs
@@ -3,7 +3,7 @@ use rayon::prelude::*;
 use log::info;
 use methylome::{IupacBase, Motif};
 use std::{
-    collections::{HashMap, HashSet},
+    collections::HashSet,
     io::{BufWriter, Write},
     path::Path,
 };
@@ -13,159 +13,6 @@ pub mod args;
 pub use args::MotifClusteringArgs;
 
 use crate::{processing::create_motifs, utils::create_output_file};
-
-#[allow(dead_code)]
-struct UnionFind {
-    pub parent: Vec<usize>,
-    pub rank: Vec<usize>,
-}
-
-#[allow(dead_code)]
-impl UnionFind {
-    pub fn new(n: usize) -> Self {
-        Self {
-            parent: (0..n).collect(),
-            rank: vec![0; n],
-        }
-    }
-
-    pub fn find(&mut self, i: usize) -> usize {
-        if self.parent[i] != i {
-            self.parent[i] = self.find(self.parent[i]);
-        }
-
-        self.parent[i]
-    }
-
-    pub fn union(&mut self, x: usize, y: usize) {
-        let rx = self.find(x);
-        let ry = self.find(y);
-
-        if rx != ry {
-            if self.rank[rx] < self.rank[ry] {
-                self.parent[rx] = ry;
-            } else if self.rank[rx] > self.rank[ry] {
-                self.parent[ry] = rx;
-            } else {
-                self.parent[ry] = rx;
-                self.rank[rx] += 1;
-            }
-        }
-    }
-}
-
-#[allow(dead_code)]
-fn edit_distance(m1: &Motif, m2: &Motif) -> usize {
-    // Line motifs according to modified base
-    let mod_position_offset = m1.mod_position as i8 - m2.mod_position as i8;
-    let mod_position_offset_abs = mod_position_offset.abs() as usize;
-    let length_diff = m1.sequence.len() as i8 - m2.sequence.len() as i8;
-    let length_diff_abs = length_diff.abs() as usize;
-
-    if mod_position_offset == 0 && length_diff == 0 {
-        return hamming_distance(&m1, &m2);
-    } else {
-        return 100;
-    }
-
-    // let mut m1_extended = m1.clone();
-    // let mut m2_extended = m2.clone();
-
-    // if mod_position_offset != 0 && length_diff == 0 {
-    //     // CCWG & CWGG
-    //     // CCWGN NCWGG
-
-    //     if mod_position_offset > 0 {
-    //         m1_extended.extend_motif_with_n(mod_position_offset_abs);
-    //         m2_extended.prepend_n(mod_position_offset_abs);
-    //     } else {
-    //         m1_extended.prepend_n(mod_position_offset_abs);
-    //         m2_extended.extend_motif_with_n(mod_position_offset_abs);
-    //     }
-
-    //     // N has a penalty of 0.5 but since an offset will always result in two Ns,
-    //     // mod_position_offset_abs is just added.
-    //     // WARN this is too hard to merge. Distance should be high!
-    //     // return hamming_distance(&m1_extended, &m2_extended) + mod_position_offset_abs;
-    //     return 100;
-    // } else if mod_position_offset == 0 && length_diff != 0 {
-    //     if length_diff > 0 {
-    //         m2_extended.extend_motif_with_n(length_diff_abs);
-    //     } else {
-    //         m1_extended.extend_motif_with_n(length_diff_abs);
-    //     }
-    //     return hamming_distance(&m1_extended, &m2_extended);
-    // } else {
-    //     // Mod position and length are different
-    //     return 100;
-    // };
-}
-
-#[allow(dead_code)]
-fn hamming_distance(s1: &Motif, s2: &Motif) -> usize {
-    if s1.sequence.len() != s2.sequence.len() {
-        panic!("Motif sequences should have the same length");
-    }
-    if s1.mod_position != s2.mod_position {
-        panic!("Motifs should have the same mod_position");
-    }
-    if s1.mod_type != s2.mod_type {
-        panic!("Motifs should have the same mod_type");
-    }
-
-    s1.sequence
-        .iter()
-        .zip(&s2.sequence)
-        .fold(0, |score, (base1, base2)| {
-            let possible_nucleotides_1 = base1.to_possible_nucleotides();
-            let possible_nucleotides_2 = base2.to_possible_nucleotides();
-
-            if possible_nucleotides_1
-                .iter()
-                .any(|x| possible_nucleotides_2.contains(x))
-            {
-                score
-            } else {
-                score + 1
-            }
-        })
-}
-
-#[allow(dead_code)]
-fn cluster_motifs(motifs: &[Motif], with_edit: bool) -> UnionFind {
-    let n = motifs.len();
-    let mut uf = UnionFind::new(n);
-
-    for i in 0..n {
-        for j in i + 1..n {
-            if motifs[i].mod_type != motifs[j].mod_type {
-                continue;
-            }
-
-            let should_union = (with_edit && edit_distance(&motifs[i], &motifs[j]) <= 1);
-            if should_union {
-                uf.union(i, j);
-            }
-        }
-    }
-
-    uf
-}
-
-
-
-
-#[allow(dead_code)]
-fn group_motifs_by_set(uf: &mut UnionFind, motifs: &[Motif]) -> HashMap<usize, Vec<Motif>> {
-    let n = motifs.len();
-    let mut map: HashMap<usize, Vec<Motif>> = HashMap::new();
-
-    for i in 0..n {
-        let root = uf.find(i);
-        map.entry(root).or_default().push(motifs[i].clone());
-    }
-    map
-}
 
 fn pick_victim(m1: &Motif, m2: &Motif) -> Motif {
     let len1 = m1.sequence_to_string().len();
@@ -180,8 +27,6 @@ fn pick_victim(m1: &Motif, m2: &Motif) -> Motif {
         m1.clone()
     }
 }
-
-
 
 fn collapse_child_motifs(motifs: &[Motif]) -> Vec<Motif> {
     let n = motifs.len();
@@ -384,65 +229,4 @@ mod tests {
 
     
 
-    #[test]
-    fn test_edit_distance_same_length_same_mod_pos() {
-        let m1 = Motif::new("GATCC", "m", 3).unwrap();
-        let m2 = Motif::new("GATCG", "m", 3).unwrap();
-        let m3 = Motif::new("GTTCT", "m", 3).unwrap();
-
-        let d1 = edit_distance(&m1, &m2);
-        assert_eq!(d1, 1);
-        let d2 = edit_distance(&m1, &m3);
-        assert_eq!(d2, 2);
-        let d3 = edit_distance(&m1, &m1);
-        assert_eq!(d3, 0);
-    }
-
-    // #[test]
-    // fn test_edit_distance_different_length_same_mod_pos() {
-    //     let m1 = Motif::new("GATCC", "m", 3).unwrap();
-    //     let m2 = Motif::new("GATC", "m", 3).unwrap();
-
-    //     let d = edit_distance(&m1, &m2);
-    //     assert_eq!(d, 0);
-    // }
-
-    // #[test]
-    // fn test_edit_distance_same_length_diff_mod_pos() {
-    //     let m1 = Motif::new("CCWG", "m", 1).unwrap();
-    //     let m2 = Motif::new("CWGG", "m", 0).unwrap();
-
-    //     let d = edit_distance(&m1, &m2);
-    //     assert_eq!(d, 1);
-    // }
-    #[test]
-    fn test_edit_distance_diff_length_diff_mod_pos() {
-        let m1 = Motif::new("CCCCWG", "m", 1).unwrap();
-        let m2 = Motif::new("CWGG", "m", 0).unwrap();
-
-        let d = edit_distance(&m1, &m2);
-        assert_eq!(d, 100);
-    }
-
-    // #[test]
-    // fn test_union_find() {
-    //     let motif1 = Motif::new("AGCT", "m", 2).unwrap();
-    //     let motif2 = Motif::new("CGAC", "m", 3).unwrap();
-    //     let motif3 = Motif::new("CGCC", "m", 2).unwrap();
-    //     let motif4 = Motif::new("CGTC", "m", 3).unwrap();
-    //     let motif5 = Motif::new("CGWC", "m", 3).unwrap();
-    //     let motif6 = Motif::new("GAGC", "m", 3).unwrap();
-    //     let motif7 = Motif::new("GTAC", "m", 3).unwrap();
-    //     let motif8 = Motif::new("GTGC", "m", 3).unwrap();
-
-    //     let motifs = vec![
-    //         motif1, motif2, motif3, motif4, motif5, motif6, motif7, motif8,
-    //     ];
-
-    //     let mut uf = cluster_motifs(&motifs, true);
-    //     let clusters = group_motifs_by_set(&mut uf, &motifs);
-
-    //     println!("{:#?}", clusters);
-    //     assert!(false);
-    // }
 }
