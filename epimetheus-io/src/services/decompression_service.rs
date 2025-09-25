e7e1736faa810a549c05373d8d86569fd8dc4514
sebdal <semoda@bio.aau.dk>
2025-09-25 09:29:11 +0000

    added parallellism for extraction

--- epimetheus-io/src/services/decompression_service.rs
+++ epimetheus-io/src/services/decompression_service.rs
@@ -1,10 +1,12 @@
 use anyhow::Result;
 use epimetheus_core::services::{domain::parallel_processer::query_pileup, traits::PileupReader};
 use log::info;
+use rayon::prelude::*;
 use std::{
     fs::File,
     io::{BufWriter, Write},
     path::Path,
+    sync::{Arc, Mutex},
 };
 
 use crate::io::readers::bgzf_bed::Reader;
@@ -15,7 +17,7 @@ pub fn extract_from_pileup(
     ls: bool,
     contigs: Vec<String>,
 ) -> Result<()> {
-    let mut reader = Reader::from_path(input)?;
+    let reader = Reader::from_path(input)?;
 
     if ls {
         let contigs_available = reader.available_contigs();
@@ -25,19 +27,46 @@ pub fn extract_from_pileup(
         return Ok(());
     }
 
-    let mut writer: Box<dyn Write> = match output {
-        Some(out) => {
-            let file = File::create(out)?;
-            Box::new(BufWriter::new(file))
-        }
-        None => Box::new(BufWriter::new(std::io::stdout())),
-    };
+    // let writer: Box<dyn Write> = match output {
+    //     Some(out) => {
+    //         let file = File::create(out)?;
+    //         Box::new(BufWriter::new(file))
+    //     }
+    //     None => Box::new(BufWriter::new(std::io::stdout())),
+    // };
+
+    // let writer = Arc::new(Mutex::new(writer));
 
     info!("Writing {} contigs.", &contigs.len());
-    for contig in contigs {
-        let records = query_pileup(&mut reader, &[contig])?;
-        for r in records {
-            writeln!(writer, "{}", r)?;
+    match output {
+        Some(out) => {
+            let file = File::create(out)?;
+            let writer = Arc::new(Mutex::new(BufWriter::new(file)));
+
+            contigs.par_iter().try_for_each(|contig| -> Result<()> {
+                let mut thread_reader = reader.clone();
+                let records = query_pileup(&mut thread_reader, &[contig.to_owned()])?;
+
+                let mut writer_guard = writer.lock().unwrap();
+                for r in records {
+                    writeln!(writer_guard, "{}", r)?;
+                }
+                Ok(())
+            })?;
+        }
+        None => {
+            let writer = Arc::new(Mutex::new(BufWriter::new(std::io::stdout())));
+
+            contigs.par_iter().try_for_each(|contig| -> Result<()> {
+                let mut thread_reader = reader.clone();
+                let records = query_pileup(&mut thread_reader, &[contig.to_owned()])?;
+
+                let mut writer_guard = writer.lock().unwrap();
+                for r in records {
+                    writeln!(writer_guard, "{}", r)?;
+                }
+                Ok(())
+            })?;
         }
     }
 
